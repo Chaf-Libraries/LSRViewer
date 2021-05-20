8a6f7bba982f15b7efd89345ca88a25d9780dd48
Wenbo Chen <chaf@mail.ustc.edu.cn>
2021-05-20 16:58:08 +0800

    update

--- source/app/app.cpp
+++ source/app/app.cpp
@@ -143,12 +143,7 @@ void Application::prepare()
 		vks::tools::exitFatal("Could not get a valid function pointer for vkCmdSetDepthTestEnableEXT", -1);
 	}
 
-	//scene = chaf::SceneLoader::LoadFromFile(*vulkanDevice, "D:/Workspace/LSRViewer/data/models/sponza_1/Sponza01.gltf", queue);
-	//scene = chaf::SceneLoader::LoadFromFile(*vulkanDevice, "D:/Workspace/LSRViewer/data/models/test/27/test.gltf", queue);
-	//scene = chaf::SceneLoader::LoadFromFile(*vulkanDevice, "D:/Workspace/LSRViewer/data/models/audi/auti03.gltf", queue);
-	//scene = chaf::SceneLoader::LoadFromFile(*vulkanDevice, "D:/Workspace/LSRViewer/data/models/bonza/Bonza4X.gltf", queue);
-	scene = chaf::SceneLoader::LoadFromFile(*vulkanDevice, "D:/Workspace/LSRViewer/data/models/sponza/sponza.gltf", queue);
-	//scene = chaf::SceneLoader::LoadFromFile(*vulkanDevice, "D:/Workspace/LSRViewer/data/models/E/E/untitled2.gltf", queue);
+	scene = chaf::SceneLoader::LoadFromFile(*vulkanDevice, "D:/Workspace/LSRViewer/data/scene/test.gltf", queue);
 
 	scene->buffer_cacher = std::make_unique<chaf::BufferCacher>(*vulkanDevice, queue);
 	scene->buffer_cacher->addBuffer(0, chaf::SceneLoader::vertex_buffer, chaf::SceneLoader::index_buffer);
